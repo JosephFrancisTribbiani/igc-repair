de2bbf18e15631ac1e9b58069472967049e71cf4
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2024-06-22 09:42:47 +0300

    Добавить поле TimeUTC + Рефакторинг + Фиксы

--- igcrepair/reader/fields.py
+++ igcrepair/reader/fields.py
@@ -1,5 +1,7 @@
+import datetime
 import re
 from abc import ABCMeta, abstractmethod
+from typing_extensions import Self
 
 from .utils import RecordFieldError
 
@@ -19,7 +21,8 @@ class RecordField(metaclass=ABCMeta):
         return len(str(self))
     
     @classmethod
-    def from_string(cls, string: str):
+    @abstractmethod
+    def from_string(cls, string: str) -> None:
         if not isinstance(string, str):
             raise RecordFieldError('Передаваемое значение должно быть типа <str>.')
         
@@ -42,14 +45,16 @@ class StringRecordField(RecordField):
             isinstance(value, str)
             and re.fullmatch(pattern=self.PATTERN, string=value)
         ):
-            raise RecordFieldError('Формат поля %s не соответствует формату %s.', self.__class__.__name__, self.PATTERN)
+            raise RecordFieldError(
+                'Формат поля {0} не соответствует формату {1}.'.format(self.__class__.__name__, self.PATTERN)
+            )
         self._value = value.upper()
 
     def __str__(self) -> str:
         return self.value
     
     @classmethod
-    def from_string(cls, string: str):
+    def from_string(cls, string: str) -> Self:
         super().from_string(string)
         return cls(value=string)
 
@@ -79,7 +84,9 @@ class IDExtension(StringRecordField):
             isinstance(value, str)
             and re.fullmatch(pattern=self.PATTERN, string=value)
         ):
-            raise RecordFieldError('Формат поля %s не соответствует формату %s.', self.__class__.__name__, self.PATTERN)
+            raise RecordFieldError(
+                'Формат поля {0} не соответствует формату {1}.'.format(self.__class__.__name__, self.PATTERN)
+            )
         self._value = value
 
 
@@ -89,3 +96,29 @@ class Validity(StringRecordField):
     to be recorded using times from the RTC).
     """
     PATTERN: re.Pattern = re.compile(pattern=r'[AV]{1}', flags=re.IGNORECASE)
+
+
+class TimeUTC(RecordField):
+    TIME_FORMAT = '%H%M%S'
+
+    def __init__(self, value: datetime.time) -> None:
+        if not isinstance(value, datetime.time):
+            raise RecordFieldError(
+                'Аттрибут класса value должен быть типа datetime.time. Передан {0}'.format(type(value))
+            )
+        self.value: datetime.time = value
+
+    def __str__(self) -> str:
+        return self.value.strftime(self.TIME_FORMAT)
+
+    @classmethod
+    def from_string(cls, string: str) -> Self:
+        super().from_string(string)
+        try:
+            if len(string) != 6:
+                raise ValueError
+            return cls(datetime.datetime.strptime(string, cls.TIME_FORMAT).time())
+        except ValueError:
+            raise RecordFieldError(
+                'Формат даты для поля {0} не соответствует формату {1}.'.format(cls.__class__.__name__, cls.TIME_FORMAT)
+            )
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,5 +1,6 @@
+import datetime
 import unittest
-from typing import Type
+from typing import Type, Any
 
 from parameterized import parameterized
 
@@ -11,6 +12,7 @@ from igcrepair.reader.fields import (
     UniqueID,
     IDExtension,
     Validity,
+    TimeUTC,
 )
 
 
@@ -28,11 +30,11 @@ class BaseTestString(unittest.TestCase):
         self.assertEqual(str(field), excepted_value)
 
     def base_test_exception(self, value: str) -> None:
-        with self.assertRaisesRegex(RecordFieldError, 'Формат поля .* не соответствует формату .*.'):
+        with self.assertRaisesRegex(RecordFieldError, r'Формат поля .* не соответствует формату .*\.'):
             self.FIELD(value)
     
     def base_test_from_string_exception(self, value: str) -> None:
-        with self.assertRaisesRegex(RecordFieldError, 'Передаваемое значение должно быть типа <str>.'):
+        with self.assertRaisesRegex(RecordFieldError, r'Передаваемое значение должно быть типа \<str\>\.'):
             self.FIELD.from_string(value)
 
 
@@ -218,3 +220,62 @@ class TestValidity(BaseTestString):
     )
     def test_from_string_exception(self, value: str) -> None:
         super().base_test_from_string_exception(value)
+
+
+class TestTimeUTC(unittest.TestCase):
+
+    @parameterized.expand(
+        [
+            (datetime.time(1, 5, 10), '010510'),
+            (datetime.time(0, 0, 0), '000000'),
+            (datetime.time(23, 59, 59), '235959'),
+        ]
+    )
+    def test(self, value: datetime.time, expected_string_value: str) -> None:
+        field = TimeUTC(value)
+        self.assertEqual(field.value, value)
+        self.assertEqual(str(field), expected_string_value)
+        self.assertEqual(field(), expected_string_value)
+
+    @parameterized.expand(
+        [
+            ('010510', datetime.time(1, 5, 10)),
+            ('000000', datetime.time(0, 0, 0)),
+            ('235959', datetime.time(23, 59, 59)),
+        ]
+    )
+    def test_from_string(self, string_value: str, expected_value) -> None:
+        field = TimeUTC.from_string(string_value)
+        self.assertEqual(field.value, expected_value)
+
+    @parameterized.expand(
+        [
+            ('abc', ),
+            (True, ),
+            (False, ),
+            (None, ),
+            (10, ),
+            (datetime.datetime(2024, 1, 1, 0, 0, 0), ),
+        ]
+    )
+    def test_exception(self, value: Any) -> None:
+        with self.assertRaisesRegex(RecordFieldError, r'Аттрибут класса value должен быть типа datetime\.time.*'):
+            TimeUTC(value)
+
+    @parameterized.expand(
+        [
+            ('123', r'Формат даты для поля .* не соответствует формату .*\.'),
+            ('abc', r'Формат даты для поля .* не соответствует формату .*\.'),
+            ('1234567', r'Формат даты для поля .* не соответствует формату .*\.'),
+            ('010560', r'Формат даты для поля .* не соответствует формату .*\.'),
+            ('016000', r'Формат даты для поля .* не соответствует формату .*\.'),
+            ('250500', r'Формат даты для поля .* не соответствует формату .*\.'),
+            (True, r'Передаваемое значение должно быть типа <str>\.'),
+            (False, r'Передаваемое значение должно быть типа <str>\.'),
+            (None, r'Передаваемое значение должно быть типа <str>\.'),
+            (10, r'Передаваемое значение должно быть типа <str>\.'),
+        ]
+    )
+    def test_from_string_exception(self, value: Any, msg: str) -> None:
+        with self.assertRaisesRegex(RecordFieldError, msg):
+            TimeUTC.from_string(value)
